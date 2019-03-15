fdcbe8b9886838db09fe564c2399906ad26ca39b
john681611 <john.harvey@marks-and-spencer.com>
2019-03-15 15:11:08 +0000

    update basic site server

--- site/app.js
+++ site/app.js
@@ -1,45 +1,41 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-const debug = require('debug')('api:server');
-const http = require('http');
 const port = process.env.PORT || '3000';
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 
-var app = express();
+var server = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(sassMiddleware({
+server.use(logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(sassMiddleware({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
     indentedSyntax: true, // true = .sass and false = .scss
     sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+server.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res) {
+server.use('*', (req, res) => {
+    res.status(404);
     res.render('notFound');
 });
 
-// error handler
-app.use(function(err, req, res) {
-    res.status(err.status || 500);
+server.use((err, req, res) =>  {
+    res.status(500);
     res.render('error');
 });
 
-const server = http.createServer(app);
-server.listen(port);
-server.on('listening', () =>  {
-    debug('Listening on ' + port);
+server.listen(port, () => {
+    process.stdout.write(`site listening on port ${port}`);
 });
--- site/config/baseTestConf.js
+++ site/config/baseTestConf.js
@@ -1,4 +1,5 @@
 const expectAssert = require('expect-assert');
 const chai = require('chai');
+chai.use(require('sinon-chai'));
 global.expect = expectAssert(chai.expect);
 global.sinon = require('sinon');
