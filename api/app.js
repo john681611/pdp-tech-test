741f4afbffbd8dc1c1748afc1ed96aa078fead62
john681611 <john.harvey@marks-and-spencer.com>
2019-03-15 13:09:58 +0000

    refactor server to be more usable

--- api/app.js
+++ api/app.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const debug = require('debug')('api:server');
 const indexRouter = require('./routes/index');
-const http = require('http');
 const port = process.env.PORT || '5000';
-const app = express();
+const server = express();
 
-app.set('port', port);
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
+server.set('port', port);
+server.use(logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(express.static(path.join(__dirname, 'public')));
+indexRouter(server);
 
-const server = http.createServer(app);
-server.listen(port);
-server.on('listening', () =>  {
-    debug('Listening on ' + port);
+server.listen(port, () => {
+    process.stdout.write(`api listening on port ${port}`);
 });
 
