e62a731c7c67fc522779fc4eb513565fe40a38dd
john681611 <john.harvey@marks-and-spencer.com>
2019-03-16 11:08:20 +0000

    Added swichign logic on page

---
+++ site/public/javascripts/index.js
@@ -0,0 +1,32 @@
+const onSizeChange = event => {
+    const elements = document.querySelectorAll('.prices');
+    [...elements].forEach(element => {
+        element.setAttribute('hidden', true);
+    });
+    document.getElementById(event.target.value).removeAttribute('hidden');
+};
+
+const onRadioChange = event => {
+    const show = event.target.value;
+    const elements = document.querySelectorAll('.swappable');
+    [...elements].forEach(element => {
+        if(element.hasAttribute('hidden')) {
+            if(element.className.includes(`colour-${show}`)) {
+                element.removeAttribute('hidden');
+            }
+        } else {
+            element.setAttribute('hidden', true);
+        }
+    });
+    const sizeSelector = document.getElementById('size');
+    sizeSelector.value = document.querySelector(`select .colour-${show}`).value;
+    sizeSelector.dispatchEvent(new Event('change'));
+
+};
+
+const radioButtons = document.querySelectorAll('.colour-selector__radio');
+[...radioButtons].forEach(radio => {
+    radio.addEventListener('change', onRadioChange);
+});
+
+document.querySelector('#size').addEventListener('change', onSizeChange);
