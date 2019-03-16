3dcc32922622e992b51d2c457719337cab907852
john681611 <john.harvey@marks-and-spencer.com>
2019-03-16 14:24:59 +0000

    Update docker and add subnet

--- dockerBuildRun.sh
+++ dockerBuildRun.sh
@@ -7,7 +7,7 @@ trap finish EXIT
 docker build --rm -f "site/Dockerfile" -t site:latest site
 docker build --rm -f "api/Dockerfile" -t api:latest api
 
-docker network create mynet
+docker network create --subnet=172.19.0.0/16 mynet
 docker run -e PASS=password -e USR=admin -e APIHOST=interview-tech-testing.herokuapp.com -p 5000:5000 --network=mynet  api & 
 docker run -e APIHOST=http://172.19.0.2:5000 -p 3000:3000 --network=mynet site
 
