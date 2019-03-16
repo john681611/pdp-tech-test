62fbbb47b0afe572be47edc7ecec34971c28da02
john681611 <john.harvey@marks-and-spencer.com>
2019-03-16 14:27:33 +0000

    fix Ips

--- dockerBuildRun.sh
+++ dockerBuildRun.sh
@@ -8,6 +8,6 @@ docker build --rm -f "site/Dockerfile" -t site:latest site
 docker build --rm -f "api/Dockerfile" -t api:latest api
 
 docker network create --subnet=172.19.0.0/16 mynet
-docker run -e PASS=password -e USR=admin -e APIHOST=interview-tech-testing.herokuapp.com -p 5000:5000 --network=mynet  api & 
-docker run -e APIHOST=http://172.19.0.2:5000 -p 3000:3000 --network=mynet site
+docker run -e PASS=password -e USR=admin -e APIHOST=interview-tech-testing.herokuapp.com -p 5000:5000 --ip 172.19.0.2 --network=mynet  api & 
+docker run -e APIHOST=http://172.19.0.2:5000 -p 3000:3000 --ip 172.19.0.3 --network=mynet site
 
