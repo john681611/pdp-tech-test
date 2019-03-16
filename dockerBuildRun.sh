0f02fb294ecabb0d61c75703b1777db5946e95b0
john681611 <john.harvey@marks-and-spencer.com>
2019-03-16 12:52:37 +0000

    added docker build and run script

---
+++ dockerBuildRun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+function finish {
+  docker network rm mynet
+}
+trap finish EXIT
+
+docker build --rm -f "site/Dockerfile" -t site:latest site
+docker build --rm -f "api/Dockerfile" -t api:latest api
+
+docker network create mynet
+docker run -e PASS=password -e USR=admin -e APIHOST=interview-tech-testing.herokuapp.com -p 5000:5000 --network=mynet  api & 
+docker run -e APIHOST=http://172.19.0.2:5000 -p 3000:3000 --network=mynet site
+
